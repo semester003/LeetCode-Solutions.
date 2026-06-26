10163c42743c072ca1993c2ce45c1bb7972e24fb
semester003 <nitinjnv0025@gmail.com>
2026-06-27 04:48:48 +0530

    Time: 0 ms (100%), Space: 18 MB (76.25%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool search(vector<int>& nums, int target) {
 
-        /* int low = 0 ;
+        int low = 0 ;
         int n = nums.size() ;
         int high = n - 1 ;
 
@@ -18,8 +18,8 @@ public:
             }
 
             // left half is sorted
-            if( nums[0] <= nums[mid] ){
-                if(nums[0] <= target && target < nums[mid] ){
+            if( nums[low] <= nums[mid] ){
+                if(nums[low] <= target && target < nums[mid] ){
                     high = mid - 1 ;
                 }
                 else{
@@ -37,45 +37,5 @@ public:
         }
         return false ;
         
-    } */
-
-
-    int low = 0;
-        int high = nums.size() - 1;
-
-        while (low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            // Target found
-            if (nums[mid] == target)
-                return true;
-
-            // Unable to determine the sorted half
-            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
-                low++;
-                high--;
-            }
-
-            // Left half is sorted
-            else if (nums[low] <= nums[mid]) {
-
-                if (nums[low] <= target && target < nums[mid])
-                    high = mid - 1;
-                else
-                    low = mid + 1;
-            }
-
-            // Right half is sorted
-            else {
-
-                if (nums[mid] < target && target <= nums[high])
-                    low = mid + 1;
-                else
-                    high = mid - 1;
-            }
-        }
-
-        return false;
     }
 };
