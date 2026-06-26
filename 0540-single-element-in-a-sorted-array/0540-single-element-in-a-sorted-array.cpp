b2bac379576fb5f5dfde4b23afe29a60aac0b206
semester003 <nitinjnv0025@gmail.com>
2026-06-27 02:30:09 +0530

    Time: 0 ms (100%), Space: 26 MB (93.77%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -5,7 +5,7 @@ public:
         int low = 0 ;
         int high = nums.size() - 1 ;
 
-        while( low < high ){
+        /* while( low < high ){  //Why "while(low < high)" Because we're shrinking the search space to one element.
             int mid = low + ( high - low )/2 ;
             // keeping mid always at even index 
             if( mid % 2 == 1 ) mid-- ;
@@ -19,8 +19,26 @@ public:
 
             }
         }
-        return nums[low] ;
+        return nums[low] ; */
 
+        while( low < high ){
+            int mid = low + ( high - low )/2 ;
+
+            if( mid % 2 == 0){  // mid is even
+
+                if( nums[mid] == nums[mid + 1]) low = mid + 2 ;
+                else{
+                    high = mid ;
+                }
+            }
+            else{ // mid is odd
+                if( nums[mid] == nums[mid+1]) high = mid - 1 ;
+                else{
+                    low = mid + 1;
+                }
+            }
+        }
+        return nums[low] ;
         
     }
 };
