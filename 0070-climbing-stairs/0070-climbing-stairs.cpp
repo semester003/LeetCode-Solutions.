4a228c6872220762619a76595f9c69670f1fe11d
semester003 <nitinjnv0025@gmail.com>
2026-07-12 01:10:28 +0530

    Time: 0 ms (100%), Space: 8.5 MB (37.21%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,11 +2,14 @@ class Solution {
 private:
     int f(int ind , vector<int>& dp){
         if(ind == 0) return 1 ;
-        if(ind == 1) return 1 ;
-        if(dp[ind] != -1) return dp[ind] ;
+        if(ind == 1) return 1 ;  // this for the sake of edge case , ie: i = 1 ,  f(i-2) = f(-1) -> runtime error 
 
+        if(dp[ind] != -1) return dp[ind] ; // memoization
+
+        // two ways , one step at a time 
         int left = f(ind - 1 , dp) ;
         dp[ind - 1] = left ;
+        // two steps at a time 
         int right = f(ind - 2 , dp) ;
         dp[ind - 2] = right ;
 
@@ -18,5 +21,13 @@ public:
         int ans  = f(n , dp) ;
         return ans ;
         
+
+        //tabulation 
+        // dp[0] = 1  , dp[1] = 1 ;
+        // for( int i = 2 ; i <= n ; i++){
+        //     dp[i] = dp[i-1] + dp[i-2] ;
+        // }
+
+        // return dp[n] ;
     }
 };
