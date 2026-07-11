ab1a4aac783ef73592baba51bbcc89a4b125cb3b
semester003 <nitinjnv0025@gmail.com>
2026-07-12 01:10:50 +0530

    Time: 0 ms (100%), Space: 8.5 MB (37.21%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -18,16 +18,16 @@ private:
 public:
     int climbStairs(int n) {
         vector<int> dp(n+1 , -1) ;
-        int ans  = f(n , dp) ;
-        return ans ;
+        // int ans  = f(n , dp) ;
+        // return ans ;
         
 
         //tabulation 
-        // dp[0] = 1  , dp[1] = 1 ;
-        // for( int i = 2 ; i <= n ; i++){
-        //     dp[i] = dp[i-1] + dp[i-2] ;
-        // }
+        dp[0] = 1  , dp[1] = 1 ;
+        for( int i = 2 ; i <= n ; i++){
+            dp[i] = dp[i-1] + dp[i-2] ;
+        }
 
-        // return dp[n] ;
+        return dp[n] ;
     }
 };
