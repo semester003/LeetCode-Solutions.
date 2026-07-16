b06d0a997d56b05df799a47a5af47ecc34144ef0
semester003 <nitinjnv0025@gmail.com>
2026-07-16 08:23:52 +0530

    Time: 0 ms (100%), Space: 11.6 MB (85.62%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -23,32 +23,55 @@ private:
 
         return dp[row][col] = left + up  ; 
 
-
     }
 
-int tabulation(int m , int n , vector<vector<int>>& obstacleGrid  ){
-        // declare dp 
-        vector<vector<int>> dp( m , vector<int> ( n , -1 )) ;
+    int tabulation(int m , int n , vector<vector<int>>& obstacleGrid  ){
+            // declare dp 
+            vector<vector<int>> dp( m , vector<int> ( n , -1 )) ;
 
-        //base case 
-        if( obstacleGrid[0][0] != 1 ) dp[0][0] = 1 ;
+            //base case 
+            // if( obstacleGrid[0][0] != 1 ) dp[0][0] = 1 ;
+
+            // express all states in for loop
+            for( int row = 0 ; row <= m-1 ; row++){
+                for(int col = 0 ; col <= n-1 ; col++){
+                    if(obstacleGrid[row][col] == 1) dp[row][col] = 0 ;   
+                    else if(row == 0 && col == 0) dp[0][0] = 1 ;
+                    else{
+                        int up = 0 ;
+                        int left = 0 ;
+                        if( row > 0 ) up = dp[row-1][col] ;
+                        if( col > 0 ) left = dp[row][col-1] ;
+
+                        dp[row][col] = up+left ;
+                    }
+                }
+            }
+            return dp[m-1][n-1] ;
+    }
+
+    int space_optimisation(int m , int n , vector<vector<int>>& obstacleGrid ){
+        // declare dp or prev row 
+        vector<int> dp(n , 0) ; // dp = [0,0,0,0,0,0]
 
         // express all states in for loop
         for( int row = 0 ; row <= m-1 ; row++){
+            vector<int> temp(n , 0) ;
             for(int col = 0 ; col <= n-1 ; col++){
-                if(obstacleGrid[row][col] == 1) dp[row][col] = 0 ;
-                else if(row == 0 && col == 0) dp[0][0] = 1 ;
+                if(obstacleGrid[row][col] == 1) temp[col] = 0 ;
+                else if(row == 0 && col == 0) temp[col] = 1 ;  // we are filling the current row 
                 else{
                     int up = 0 ;
                     int left = 0 ;
-                    if( row > 0 ) up = dp[row-1][col] ;
-                    if( col > 0 ) left = dp[row][col-1] ;
+                    if( row > 0 ) up = dp[col] ; // value of 'col' from previous row
+                    if( col > 0 ) left = temp[col-1] ;  // value of "col-1" from current row
 
-                    dp[row][col] = up+left ;
+                    temp[col] = up + left ;
                 }
             }
+            dp = temp ;  // after filling the current row , make it the previous row and go to next row
         }
-        return dp[m-1][n-1] ;
+        return dp[n-1] ;
     }
 
 public:
@@ -59,7 +82,7 @@ public:
 
         //vector<vector<int>> dp( m , vector<int> (n , -1)) ;
 
-        int ans = tabulation( m , n , obstacleGrid ) ;
+        int ans = space_optimisation( m , n , obstacleGrid ) ;
 
         return ans ;
         
