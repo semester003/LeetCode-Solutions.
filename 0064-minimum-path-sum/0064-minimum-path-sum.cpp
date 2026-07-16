62278f63aa95b8e026f666b4da2584320b2c81ef
semester003 <nitinjnv0025@gmail.com>
2026-07-16 16:50:22 +0530

    Time: 3 ms (30.26%), Space: 16.3 MB (61.95%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -15,19 +15,39 @@ private:
         long long up   = grid[row][col] + 1LL * f( row-1 , col , grid , dp ) ;
         long long left = grid[row][col] + 1LL * f( row , col-1 , grid , dp ) ;
 
-        return dp[row][col] = min( up , left ) ;
-
-        
+        return dp[row][col] = min( up , left ) ;        
     }
+
+    int tabulation( int m , int n , vector<vector<int>>& grid ){
+        vector<vector<int>> dp( m , vector<int> ( n , 0)) ;
+        // base case or first cell initialization
+        dp[0][0] = grid[0][0] ;
+
+        for( int row = 0 ; row <= m-1 ; row++){
+            for( int col = 0 ; col <= n-1 ; col++){
+                if( row == 0 &&  col == 0 ) continue ;
+                else{
+                    int up = INT_MAX ;
+                    int left = INT_MAX ;
+                    if( row > 0 ) up = grid[row][col] + 1LL * dp[row-1][col] ;
+                    if( col > 0 ) left = grid[row][col] + 1LL * dp[row][col-1] ;
+
+                    dp[row][col] = min( up , left ) ;
+                }
+            }
+        }
+        return dp[m-1][n-1] ;
+    }
+
 public:
     int minPathSum(vector<vector<int>>& grid) {
 
         int m = grid.size() ;
         int n = grid[0].size() ;
 
-        vector<vector<int>> dp( m , vector<int> ( n , -1)) ;
+        //vector<vector<int>> dp( m , vector<int> ( n , -1)) ;
 
-        int ans = f( m-1 , n-1 , grid , dp ) ;
+        int ans = tabulation( m, n, grid ) ;
         return ans ;
         
     }
