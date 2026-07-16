ca313adea2b044a3d3d22df155d18d420a8f8eba
semester003 <nitinjnv0025@gmail.com>
2026-07-16 07:13:28 +0530

    Time: 2 ms (20.87%), Space: 9.4 MB (54.25%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    int f(int row , int col , int m , int n  , vector<vector<int>>& dp ){
+    int f(int row , int col , vector<vector<int>>& dp ){
         // base case
         if(row == 0 && col == 0){
             return  1 ;
@@ -10,9 +10,9 @@ private:
         // memoization
         if( dp[row][col] != -1) return dp[row][col] ;
             
-        int left = f( row , col-1 , m , n , dp );
+        int left = f( row , col-1 , dp );
         
-        int up =  f( row-1 , col , m , n , dp );
+        int up =  f( row-1 , col , dp );
         
 
         return dp[row][col] = left + up  ; 
@@ -20,10 +20,58 @@ private:
 
     }
 
+    int tabulation(int m , int n ){
+        // declare dp 
+        vector<vector<int>> dp( m , vector<int> ( n , -1 )) ;
+        // base case 
+        dp[0][0] = 1 ;
+
+        // express all states in for loop
+        for( int row = 0 ; row <= m-1 ; row++){
+            for(int col = 0 ; col <= n-1 ; col++){
+                if(row == 0 && col == 0) dp[row][col] = 1 ;
+                else{
+                    int up = 0 ;
+                    int left = 0 ;
+                    if( row > 0 ) up = dp[row-1][col] ;
+                    if( col > 0 ) left = dp[row][col-1] ;
+
+                    dp[row][col] = up+left ;
+                }
+            }
+        }
+        return dp[m-1][n-1] ;
+    }
+
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp( m , vector<int> ( n , -1 )) ;
-        int ans  = f( m-1 , n-1 , m , n , dp  ) ;
+        // vector<vector<int>> dp( m , vector<int> ( n , -1 )) ;
+        int ans  = tabulation( m, n ) ;
         return ans ;
+
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
