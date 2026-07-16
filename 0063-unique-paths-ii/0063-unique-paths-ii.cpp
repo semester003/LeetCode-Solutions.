35319ee1addcd98d43c96e9590ea4c867f35892e
semester003 <nitinjnv0025@gmail.com>
2026-07-16 08:15:18 +0530

    Time: 0 ms (100%), Space: 11.8 MB (54.79%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -3,10 +3,13 @@ private:
     int f(int row , int col , vector<vector<int>>& obstacleGrid ,  vector<vector<int>>& dp ){
         // base case
         if(row == 0 && col == 0){
-            if( obstacleGrid[row][col] == 1 ) return 0 ;
+            if( obstacleGrid[row][col] == 1 ) return 0 ;  // will handle the base case where obstacleGrid contain only one element
             return  1 ;
         }
-        if( row <0  || col < 0) return 0 ;
+
+        // in case we go outside of the grid
+        if( row < 0  || col < 0) return 0 ;
+
         // if there is the any obstacle then can't include this path
         if( obstacleGrid[row][col] == 1 ) return 0 ;
 
@@ -23,15 +26,45 @@ private:
 
     }
 
+int tabulation(int m , int n , vector<vector<int>>& obstacleGrid  ){
+        // declare dp 
+        vector<vector<int>> dp( m , vector<int> ( n , -1 )) ;
+
+        if( m == 1 && n == 1 ){
+            if( obstacleGrid[0][0] == 1 ) dp[0][0] = 0 ;
+            else dp[0][0] = 1 ;
+        }
+
+        // base case 
+        // if( obstacleGrid[0][0] != 1 ) dp[0][0] = 1 ;
+
+        // express all states in for loop
+        for( int row = 0 ; row <= m-1 ; row++){
+            for(int col = 0 ; col <= n-1 ; col++){
+                if(obstacleGrid[row][col] == 1) dp[row][col] = 0 ;
+                else if(row == 0 && col == 0) dp[0][0] = 1 ;
+                else{
+                    int up = 0 ;
+                    int left = 0 ;
+                    if( row > 0 ) up = dp[row-1][col] ;
+                    if( col > 0 ) left = dp[row][col-1] ;
+
+                    dp[row][col] = up+left ;
+                }
+            }
+        }
+        return dp[m-1][n-1] ;
+    }
+
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
 
         int m = obstacleGrid.size() ;
         int n = obstacleGrid[0].size() ;
 
-        vector<vector<int>> dp( m , vector<int> (n , -1)) ;
+        //vector<vector<int>> dp( m , vector<int> (n , -1)) ;
 
-        int ans = f( m-1 , n-1 , obstacleGrid , dp ) ;
+        int ans = tabulation( m , n , obstacleGrid ) ;
 
         return ans ;
         
