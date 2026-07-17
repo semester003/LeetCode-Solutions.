ab4981856069f6eac815fb45d8d7686ce77bbe32
semester003 <nitinjnv0025@gmail.com>
2026-07-17 08:34:26 +0530

    Time: 1 ms (20.26%), Space: 13.4 MB (26.01%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    int f( int i , int j , vector<vector<int>>& triangle  , int n , vector<vector<int>>& dp ){
+        
+        // base case 
+        if( i == n-1 ) return triangle[n-1][j] ;
+        // memoization
+        if( dp[i][j] != 1e9 ) return dp[i][j] ;
+
+        // all stuffs
+        int down = triangle[i][j] + f( i+1 , j , triangle , n , dp) ;
+        int diag = triangle[i][j] + f( i+1 , j+1 , triangle , n , dp) ;
+
+        return dp[i][j] = min( down , diag ) ;
+
+    }
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size() ;
+        // cant initialize dp with -1 this time cause triangle contain negatives as well ,
+        vector<vector<int>> dp( n , vector<int>  ( n , 1e9)) ;   
+
+        int ans = f( 0 , 0 , triangle , n , dp ) ;
+        return ans ;
+    }
+};
