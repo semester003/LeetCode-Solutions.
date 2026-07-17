1d1571a44e76334048b873c4564dc42284d18bef
semester003 <nitinjnv0025@gmail.com>
2026-07-17 08:56:24 +0530

    Time: 0 ms (100%), Space: 13.3 MB (59.23%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -14,13 +14,37 @@ private:
         return dp[i][j] = min( down , diag ) ;
 
     }
+
+    int tabulation(vector<vector<int>>& triangle , int n ){  
+
+        vector<vector<int>> dp( n , vector<int> ( n , INT_MAX)) ;
+        // base case 
+        for( int j = 0 ; j <= n-1 ; j++ ){
+            dp[n-1][j] = triangle[n-1][j] ;
+        }
+
+        for( int i = n-2 ; i >= 0 ; i--){
+            for( int j = i ; j >= 0 ; j--){
+
+                int down = triangle[i][j] + dp[i+1][j] ;
+                int diag = triangle[i][j] + dp[i+1][j+1] ;
+
+                dp[i][j] = min( down , diag ) ;
+            }
+        }
+
+        return dp[0][0] ;   // always return the the index where you started your recursion from 
+
+    }
+
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size() ;
         // cant initialize dp with -1 this time cause triangle contain negatives as well ,
-        vector<vector<int>> dp( n , vector<int>  ( n , INT_MAX)) ;   
+        //vector<vector<int>> dp( n , vector<int>  ( n , INT_MAX)) ;   
 
-        int ans = f( 0 , 0 , triangle , n , dp ) ;
+        //int ans = f( 0 , 0 , triangle , n , dp ) ;
+        int ans = tabulation( triangle , n ) ;
         return ans ;
     }
 };
