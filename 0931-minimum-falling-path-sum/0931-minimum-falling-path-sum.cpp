060743a9a60eba3521fd8f699bd9f4d679e1a342
semester003 <nitinjnv0025@gmail.com>
2026-07-17 11:23:24 +0530

    Time: 0 ms (100%), Space: 15.1 MB (24.78%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -4,17 +4,17 @@ private:
 
         // base cases 
         // incase a coordinate go out of bound
-        if( j < 0 || j > n-1 ) return 1e9 ;
-        if( i == n-1 ) return matrix[n-1][j] ;
+        if( j < 0 || j > n-1 ) return 1e9 ;    // why this base case is before the second one ?
+        // reach the last row 
+        if( i == 0 ) return matrix[0][j] ;
 
         // memoization
         if( dp[i][j] != 1e9 ) return dp[i][j] ;
 
         // all stuffs
-        int down = matrix[i][j] + f( i+1 , j , matrix , n , dp) ;
-        // left and right can go out of bound , runtime error    
-        int left = matrix[i][j] + f( i+1 , j-1 , matrix , n , dp) ;
-        int right = matrix[i][j] + f( i+1 , j+1 , matrix , n , dp) ;
+        int down = matrix[i][j] + f( i-1 , j , matrix , n , dp) ; 
+        int left = matrix[i][j] + f( i-1 , j-1 , matrix , n , dp) ;
+        int right = matrix[i][j] + f( i-1 , j+1 , matrix , n , dp) ;
 
         return dp[i][j] =  min( down , min(left , right) ) ;
     }
@@ -22,9 +22,11 @@ public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size() ;
         vector<vector<int>> dp( n , vector<int>(n , 1e9 )) ;
+
         int ans = INT_MAX ;
+        // will be calling the recurstion from each element of the last row 
         for( int j = 0 ; j <= n-1 ; j++){
-            int path_sum = f( 0 , j , matrix  , n , dp ) ;
+            int path_sum = f( n-1 , j , matrix  , n , dp ) ;
             ans = min( ans , path_sum ) ;
         }
 
