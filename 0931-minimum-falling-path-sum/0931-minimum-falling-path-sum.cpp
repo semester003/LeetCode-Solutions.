d332ee2a2aad1600f5ccab3068be3db32ee6c276
semester003 <nitinjnv0025@gmail.com>
2026-07-17 12:00:05 +0530

    Time: 0 ms (100%), Space: 15.5 MB (15.6%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -12,11 +12,38 @@ private:
         if( dp[i][j] != 1e9 ) return dp[i][j] ;
 
         // all stuffs
-        int down = matrix[i][j] + f( i-1 , j , matrix , n , dp) ; 
+        int up = matrix[i][j] + f( i-1 , j , matrix , n , dp) ; 
         int left = matrix[i][j] + f( i-1 , j-1 , matrix , n , dp) ;
         int right = matrix[i][j] + f( i-1 , j+1 , matrix , n , dp) ;
 
-        return dp[i][j] =  min( down , min(left , right) ) ;
+        return dp[i][j] =  min( up , min(left , right) ) ;
+    }
+    int tabulation( int n , vector<vector<int>>& matrix ){
+        vector<vector<int>> dp( n , vector<int>(n , 1e9 )) ;
+        // base cases 
+        for(int j = 0 ; j <= n-1 ; j++){
+            dp[0][j] = matrix[0][j] ;
+        }
+
+        for( int i = 1 ; i <= n-1 ; i++){
+            for( int j = 0 ; j <= n-1 ; j++){
+                int up = matrix[i][j] + dp[i-1][j] ;
+                int left = 1e9 ;
+                if( j > 0 ) left = matrix[i][j] + dp[i-1][j-1] ;
+                int right = 1e9 ;
+                if( j < n-1 ) right = matrix[i][j] + dp[i-1][j+1] ;
+
+                dp[i][j] =  min( up , min(left , right) ) ;
+            }
+        }
+        
+        int ans = INT_MAX ;
+        for( int j = 0 ; j <= n-1 ; j++){
+            int path_sum = dp[n-1][j] ;
+            ans = min( ans , path_sum ) ;
+        }
+        return ans ; 
+
     }
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
@@ -25,12 +52,14 @@ public:
 
         int ans = INT_MAX ;
         // will be calling the recurstion from each element of the last row 
-        for( int j = 0 ; j <= n-1 ; j++){
-            int path_sum = f( n-1 , j , matrix  , n , dp ) ;
-            ans = min( ans , path_sum ) ;
-        }
+        // for( int j = 0 ; j <= n-1 ; j++){
+        //     int path_sum = f( n-1 , j , matrix  , n , dp ) ;
+        //     ans = min( ans , path_sum ) ;
+        // }
 
-        return ans ;
+        
+
+        return tabulation( n , matrix ) ;
         
     }
 };
