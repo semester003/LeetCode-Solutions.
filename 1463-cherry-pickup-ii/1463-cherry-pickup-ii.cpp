997c4b42f291b9d0a3722bfc167c3e36de0213de
semester003 <nitinjnv0025@gmail.com>
2026-07-19 17:39:47 +0530

    Time: 87 ms (9.32%), Space: 20.8 MB (74.98%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -25,14 +25,53 @@ private:
         return dp[i][j1][j2] =  maxi ;
     }
 
+    int tabulation( vector<vector<int>>& grid , int m , int n ){
+        vector<vector<vector<int>>> dp( m , vector<vector<int>> ( n , vector<int> ( n , -1))) ;
+
+        // base case
+        for( int j1 = 0 ; j1 < n ; j1++){
+            for( int j2 = 0 ; j2 < n ; j2++){
+                if(j1 == j2) dp[m-1][j1][j2] =  grid[m-1][j1] ; 
+                else dp[m-1][j1][j2] =  grid[m-1][j1] + grid[m-1][j2] ; 
+            }
+        }
+
+        // express every states in for loops 
+        for( int i = m-2 ; i >= 0 ; i--){
+            for( int j1 = 0 ; j1 <= n-1 ; j1++){
+                for( int j2 = 0 ; j2 <= n-1 ; j2++){
+
+                    int maxi = -1e9 ;
+                    for( int dj1 = -1 ; dj1 <= 1 ; dj1++ ){
+                        for( int dj2 = -1 ; dj2 <= 1 ; dj2++ ){
+                            
+                            if( j1+dj1 < 0 || j1+dj1 >= n || j2+dj2 < 0 || j2+dj2 >=n ) continue ;
+                            if( j1 == j2 ) maxi = max( maxi , grid[i][j1] + dp[i+1][j1+dj1][j2+dj2] ) ;
+                            else{
+                                maxi = max( maxi , grid[i][j1] + grid[i][j2] + dp[i+1][j1+dj1][j2+dj2] ) ;
+                            }
+                
+                        }
+                    }
+                    dp[i][j1][j2] = maxi ;
+                }
+            }
+        }
+        return dp[0][0][n-1] ; // always return the state you strated in recursion with 
+    }
+
+
 public:
     int cherryPickup(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<vector<int>>> dp( m , vector<vector<int>> ( n , vector<int> ( n , -1))) ;
+        // vector<vector<vector<int>>> dp( m , vector<vector<int>> ( n , vector<int> ( n , -1))) ;
 
-        int ans = f( 0 , 0 , n-1 , grid , n , m , dp ) ;
+        // int ans = f( 0 , 0 , n-1 , grid , n , m , dp ) ;
+        // return ans ;
+
+        int ans = tabulation(grid , m , n ) ;
         return ans ;
 
         
